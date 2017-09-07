94dd253d04eb54fbf38024b1af258f891048a284
Dominick Accattato <dominick@infrared5.com>
2017-09-07 14:33:54 -0400

    Added initial project structure for the 'application-level-logging- video guide

---
+++ 06-application-level-logging/example/src/main/java/com/red5pro/example/ExampleApplication.java
@@ -0,0 +1,46 @@
+package com.red5pro.example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.red5.server.adapter.MultiThreadedApplicationAdapter;
+import org.red5.server.api.IConnection;
+import org.red5.server.api.Red5;
+import org.red5.server.api.scope.IScope;
+import org.red5.server.api.stream.IBroadcastStream;
+
+/**
+ * @author daccattato
+ *
+ */
+public class ExampleApplication extends MultiThreadedApplicationAdapter {
+
+	@Override
+	public boolean appConnect(IConnection conn, Object[] params) {
+		log.info("appConnect");
+		return super.appConnect(conn, params);
+	}
+
+	@Override
+	public void appDisconnect(IConnection conn) {
+		log.info("appDisconnect");
+		super.appDisconnect(conn);
+	}
+
+	@Override
+	public boolean appStart(IScope app) {
+		log.info("appStart");
+		
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+
+		Runnable task = () -> log.debug("Scheduling: " + System.nanoTime());
+
+		int initialDelay = 0;
+		int period = 1;
+		executor.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+		
+		return super.appStart(app);
+	}
+
+}
